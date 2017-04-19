7c5aca2650349655bb01ed1b96703f5a551424aa
jibo <jibo@outlook.com>
2017-04-19 11:40:01 +0800

    Change pagination

--- upload/system/library/pagination.php
+++ upload/system/library/pagination.php
@@ -33,12 +33,12 @@ class Pagination {
 		$output = '<ul class="pagination">';
 
 		if ($page > 1) {
-			$output .= '<li><a href="' . str_replace(array('&amp;page={page}', '&page={page}'), '', $this->url) . '">' . $this->text_first . '</a></li>';
+			$output .= '<a href="' . str_replace(array('&amp;page={page}', '&page={page}'), '', $this->url) . '"><li>' . $this->text_first . '</li></a>';
 			
 			if ($page - 1 === 1) {
-				$output .= '<li><a href="' . str_replace(array('&amp;page={page}', '&page={page}'), '', $this->url) . '">' . $this->text_prev . '</a></li>';
+				$output .= '<a href="' . str_replace(array('&amp;page={page}', '&page={page}'), '', $this->url) . '"><li>' . $this->text_prev . '</li></a>';
 			} else {
-				$output .= '<li><a href="' . str_replace('{page}', $page - 1, $this->url) . '">' . $this->text_prev . '</a></li>';
+				$output .= '<a href="' . str_replace('{page}', $page - 1, $this->url) . '"><li>' . $this->text_prev . '</li></a>';
 			}
 		}
 
@@ -66,17 +66,17 @@ class Pagination {
 					$output .= '<li class="active"><span>' . $i . '</span></li>';
 				} else {
 					if ($i === 1) {
-					$output .= '<li><a href="' . str_replace(array('&amp;page={page}', '&page={page}'), '', $this->url) . '">' . $i . '</a></li>';
+					$output .= '<a href="' . str_replace(array('&amp;page={page}', '&page={page}'), '', $this->url) . '"><li>' . $i . '</li></a>';
 					} else {
-						$output .= '<li><a href="' . str_replace('{page}', $i, $this->url) . '">' . $i . '</a></li>';
+						$output .= '<a href="' . str_replace('{page}', $i, $this->url) . '"><li>' . $i . '</li></a>';
 					}
 				}
 			}
 		}
 
 		if ($page < $num_pages) {
-			$output .= '<li><a href="' . str_replace('{page}', $page + 1, $this->url) . '">' . $this->text_next . '</a></li>';
-			$output .= '<li><a href="' . str_replace('{page}', $num_pages, $this->url) . '">' . $this->text_last . '</a></li>';
+			$output .= '<a href="' . str_replace('{page}', $page + 1, $this->url) . '"><li>' . $this->text_next . '</li></a>';
+			$output .= '<a href="' . str_replace('{page}', $num_pages, $this->url) . '"><li>' . $this->text_last . '</li></a>';
 		}
 
 		$output .= '</ul>';
